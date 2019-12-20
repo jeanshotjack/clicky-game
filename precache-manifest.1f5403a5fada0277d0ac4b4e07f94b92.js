8a6745eef1f1bb11aa839afe26902143d22f866e
jeanshotjack <dshalenkova@gmail.com>
2019-12-19 22:05:56 -0500

    Updates

---
+++ precache-manifest.1f5403a5fada0277d0ac4b4e07f94b92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0e276e9b25c95d6b2d7a256e5acd9ac",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "03d967328bce3f4d2b97",
+    "url": "/clicky-game/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "da2148e6f4bb4e003925",
+    "url": "/clicky-game/static/js/2.4113a606.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky-game/static/js/2.4113a606.chunk.js.LICENSE"
+  },
+  {
+    "revision": "03d967328bce3f4d2b97",
+    "url": "/clicky-game/static/js/main.c930e28f.chunk.js"
+  },
+  {
+    "revision": "668f4b8c30abb5232a2e",
+    "url": "/clicky-game/static/js/runtime-main.a1c47037.js"
+  }
+]);
