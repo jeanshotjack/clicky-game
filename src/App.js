90eca0bc6962b8c10b848b22ab35dfe8a896acc1
jeanshotjack <dshalenkova@gmail.com>
2019-12-19 20:41:19 -0500

    got logic to work I think

--- src/App.js
+++ src/App.js
@@ -13,24 +13,55 @@ class App extends React.Component {
     highscore: 0
   };
 
+  gameOver = () => {
+    if (this.state.score > this.state.highscore) {
+      this.setState({ highscore: this.state.score }, function () {
+        console.log(this.state.highscore);
+      });
+    }
+    this.state.cards.forEach(card => {
+      card.count = 0;
+    });
+    alert(`Game Over :( \nscore: ${this.state.score}`);
+    this.setState({ score: 0 });
+    return true;
+  }
 
+  counter = id => {
+    this.state.cards.find((j, i) => {
+      if (j.id === id) {
+        if (cards[i].count === 0) {
+          cards[i].count = cards[i].count + 1;
+          this.setState({ score: this.state.score + 1 }, function () {
+            console.log(this.state.score);
+          });
+          this.state.cards.sort(() => Math.random() - 0.5)
+          return true;
+        } else {
+          this.gameOver();
+        }
+      }
+    });
+  }
 
   render() {
 
     return (
       <Wrapper>
-        <Header score={this.state.score} highscore={this.state.highscore}>Memory Game</Header>
+        <Header score={this.state.score} highscore={this.state.highscore}>
+          <h1>Memory Game</h1>
+        </Header>
 
         <div className="container">
           <div className="row">
-              {this.state.cards.map(card => (
-                <Card
-
-                  id={card.id}
-                  key={card.id}
-                  image={card.image}
-                />
-              ))}
+            {this.state.cards.map(card => (
+              <Card
+                counter={this.counter}
+                id={card.id}
+                key={card.id}
+                image={card.image}
+              />
+            ))}
           </div>
         </div>
       </Wrapper>
